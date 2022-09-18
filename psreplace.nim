ad33008ea2dc6ec542bd75ec5b6bea4107ff9f0b
d0rksh <100582637+d0rksh@users.noreply.github.com>
2022-09-18 19:28:25 +0530

    error handling

--- psreplace.nim
+++ psreplace.nim
@@ -1,6 +1,11 @@
 import urlly
-import os
+import httpclient
 import strutils
+import sequtils
+import threadpool
+import terminal
+import osproc
+var thread_count = countProcessors() * 2
 proc concat_query(path:string,query:seq[(string,string)]):string=
     var build_url = path
     for index,data in query:
@@ -20,15 +25,42 @@ proc process_path(path:seq[string],payload:string,query:seq[(string,string)],sc:
             var front = path[1..index-1]
             var back =  path[index+1..path.len()-1]
             if index == path.len()-1:
-                 echo sc&"://"&host&"/"&concat_query(front.join("/")&"/"&encodeUrlComponent(payload),query)
+                 var url = sc&"://"&host&"/"&concat_query(front.join("/")&"/"&encodeUrlComponent(payload),query)
+                 try:
+                    var client = newHttpClient()
+                    var resp = client.request(url)
+                    var body =  resp.body()
+                    if "pXSS" in body:
+                        stdout.setForeGroundColor(fgRed)
+                        stdout.styledWriteLine(fgDefault, url & " ", fgRed,styleBright, "[pXSS Reflected]",)
+                        stdout.resetAttributes()
+                 except:
+                        discard
+
+
             else:
-                echo sc&"://"&host&"/"&concat_query(front.join("/")&"/"&encodeUrlComponent(payload)&"/"&back.join("/"),query)
+                var url =  sc&"://"&host&"/"&concat_query(front.join("/")&"/"&encodeUrlComponent(payload)&"/"&back.join("/"),query)
+                try:
+                    var client = newHttpClient()
+                    var resp = client.request(url)
+                    var body = resp.body()
+                    if "pXSS" in body:
+                        stdout.setForeGroundColor(fgRed)
+                        stdout.styledWriteLine(fgDefault, url & " ", fgRed,styleBright, "[pXSS Reflected]",styleBright )
+                        stdout.resetAttributes()
+                except:
+                        discard
 var input = readAll(stdin)
 var all_url = split(input,"\n")
-for url in all_url:
-     var purl = parseUrl(url)
-     var splited = purl.path().split("/")
-     var payload = "FUZZ"
-     if paramCount() >= 1:
-         payload = paramStr(1)
-     process_path(splited,payload,purl.query,purl.scheme,purl.hostname)
+var sub_seq = all_url.distribute(thread_count)
+
+proc starter_func(urls: seq[string]):void=
+    for url in urls:
+        var purl = parseUrl(url)
+        var splited = purl.path().split("/")
+        var payload = "pXSS"
+        process_path(splited,payload,purl.query,purl.scheme,purl.hostname)
+
+for url in sub_seq:
+     spawn starter_func(url)
+sync()
